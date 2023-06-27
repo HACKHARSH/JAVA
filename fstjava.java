5406806617ee96b456810d1fe172958756f055fa
HACKHARSH <hrsh03mhjn@gmail.com>
2023-06-27 19:29:40 +0530

    operators

--- fstjava.java
+++ fstjava.java
@@ -1,3 +1,13 @@
-public class Main {
-    system.out.print("hello")
+class Fstjava
+{
+    public static void main(String args[])
+    {
+        //literals
+        int num1=3;
+        int num2=5;
+        int res = num1 + num2;
+        System.out.println(res);
+
+    }
+    
 }
