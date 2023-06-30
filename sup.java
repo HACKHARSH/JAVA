bfad658c055ad698a6d2a7e81f40e7de92ac1a17
HACKHARSH <hrsh03mhjn@gmail.com>
2023-07-01 00:12:12 +0530

    super method and inheritance

---
+++ sup.java
@@ -0,0 +1,25 @@
+
+class A{
+    public A(){
+        System.out.println("in A");
+    }
+
+
+}
+
+class B extends A{
+    public B(){
+        System.out.println("in B");
+    }
+
+}
+
+public class sup {
+    public static void main(String args[]){
+
+        B obj = new B();
+
+
+    }
+    
+}
