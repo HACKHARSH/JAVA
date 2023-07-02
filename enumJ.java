9cf9a6cdf4531b8798173a8ab6391e42579ef027
HACKHARSH <hrsh03mhjn@gmail.com>
2023-07-03 02:57:48 +0530

    enum update

--- enumJ.java
+++ enumJ.java
@@ -2,16 +2,38 @@ enum Status{
     Running, Failed, Pending, Success,Error;
 }
 
+enum Laptop{
+    Macbook(2000),Dell(1500),ASUS(1800);
+
+    private int price;
+
+    private Laptop(int price){
+        this.price = price;
+
+
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+  
+}
+
 public class enumJ {
      public static void main(String args[]){
 
        Status s= Status.Failed;
        System.out.println(s.ordinal());
 
-       Status []ss= Status.values();
+    //    Status []ss= Status.values();
 
-       for(Status show :ss){
-        System.out.println(show);
+       for(Laptop show : Laptop.values()){
+        System.out.println(show + " : "+show.getPrice() );
        }
 
     }
