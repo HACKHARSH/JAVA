b30c438fa527d1ae56aeab393f85657d5682e5e2
HACKHARSH <hrsh03mhjn@gmail.com>
2023-07-05 14:39:47 +0530

    aa

--- aisehi.java
+++ aisehi.java
@@ -3,7 +3,20 @@ public class aisehi {
     public static void main(String args[]){
 
         String a = "ab";
-        System.out.println(a.charAt(0));
+        //System.out.println(a.length());
+        //System.out.println(a.charAt(0));
+
+        char[] ch = a.toCharArray();
+        for(int i = 0; i<ch.length; i++){
+            System.out.println(ch[i]);
+        }
+
+        
+
+        if(a.charAt(0) == 'a'){
+            System.out.println("worj=king");
+
+        }
     }
     
 }
