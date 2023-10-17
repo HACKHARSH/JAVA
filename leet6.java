62f6ce787dd5a51d902bdd3862ca16e05a83ade7
HACKHARSH <hrsh03mhjn@gmail.com>
2023-10-17 22:10:54 +0530

    modified:   leet6.java

--- leet6.java
+++ leet6.java
@@ -1,6 +1,21 @@
 public class leet6 {
     public static void main(String[] args) {
         
+        // int [] arr = new int [10];
+
+        // for(int i=0;i<=arr.length -1;i++){
+        //     arr[i] = 2*(i+1);
+
+        // }
+        // for(int i=0;i<=arr.length -1;i++){
+        //     System.out.println(arr[i]);
+            
+        // }
+
+
+        
+
+        
+        
     }
-    
 }
