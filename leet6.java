54b52fc0bbc98169c14bf2aa93e8f766dc983723
HACKHARSH <hrsh03mhjn@gmail.com>
2023-10-18 10:09:59 +0530

    ep09

--- leet6.java
+++ leet6.java
@@ -1,16 +1,47 @@
 public class leet6 {
     public static void main(String[] args) {
         
-        // int [] arr = new int [10];
+        int [] arr = new int [10];
 
-        // for(int i=0;i<=arr.length -1;i++){
-        //     arr[i] = 2*(i+1);
+        for(int i=0;i<=arr.length -1;i++){
+            arr[i] = 2*(i+1);
 
-        // }
-        // for(int i=0;i<=arr.length -1;i++){
-        //     System.out.println(arr[i]);
+        }
+        for(int i=0;i<=arr.length -1;i++){
+            System.out.println(arr[i]);
             
-        // }
+        }
+
+
+        // printing the array in reverse
+        
+        for(int i=arr.length -1;i>=0;i--){
+            System.out.println(arr[i]);
+            
+        }
+
+
+
+        int min = 100, max =0;
+
+        for(int i=0;i<=arr.length -1;i++){
+            //System.out.println(arr[i]);
+            if (min> arr[i]){
+                min = arr[i];
+            }
+
+            if (max < arr[i]){
+                max= arr[i];
+            }
+        }
+
+        System.out.println("min is " + min);
+        System.out.println("max is " + max);
+
+
+
+
+
 
 
         
