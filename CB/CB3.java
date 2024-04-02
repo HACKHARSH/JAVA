ca72aa66863723bfe2cb16165827d95979fed426
HACKHARSH <hrsh03mhjn@gmail.com>
2024-04-02 14:09:36 +0530

    vs pull

--- CB/CB3.java
+++ CB/CB3.java
@@ -3,5 +3,6 @@ package CB;
 public class CB3 {
     public static void main(String[] args) {
         System.out.println("setup is completed InteleJ idea");
+        System.out.println("forwarding to vs code");
     }
 }
