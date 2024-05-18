2967be43a9b4fe49e742c4c6a3d4a0fba5515f0b
HACKHARSH <hrsh03mhjn@gmail.com>
2024-05-18 19:26:01 +0530

    18may

---
+++ CBJAVA/DataTypes1.java
@@ -0,0 +1,20 @@
+package CBJAVA;
+
+import java.util.Scanner;
+
+public class DataTypes1 {
+    public static void main(String[] args) {
+        byte b = (byte)300;
+        short s = 191;
+        int i = 159;
+        long l = 10900000000000000l;
+        System.out.println(b);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+        char vl =  'a';
+        
+        
+    }
+    
+}
--- CBJAVA/start.java
+++ CBJAVA/start.java
@@ -1,5 +1,27 @@
 package CBJAVA;
 
 public class start {
-    
+    public static void main(String[] args) {
+        System.out.println("hello");
+        // this is only applicabe when the value of source or dest is 10
+        int src = 10;
+        int dest =2;
+
+
+        int sum =0;
+        int mul=1;
+        int n=1011110;
+        int rem;
+
+        while(n>0){
+            rem=n%src;
+            sum = sum + (rem*mul);
+            mul= mul*dest;
+            n=n/src;
+
+        }
+        System.out.println(sum);
+
+
+    }
 }
